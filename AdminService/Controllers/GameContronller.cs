5bdc039ede9c68fff12cd1e92c4aca5100767ad4
Noneleft <noneleftzyx@gmail.com>
2020-01-12 20:30:37 +0000

    Index Page for Games (Ongoing Games)

--- AdminService/Controllers/GameContronller.cs
+++ AdminService/Controllers/GameContronller.cs
@@ -16,9 +16,30 @@ namespace AdminService.Controllers
 {
     public class GameController: Controller
     {
+        IConfiguration _configuration;
+        public GameController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         [Route("/game")]
-        public async Task<IActionResult> Index(){
-            return View();
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            string baseUrl = _configuration["ServerAddress:StorageServerAddress"] + "/api/game/list/1";
+            List<Game> data = new List<Game>();
+            List<Game> ongoingGames = new List<Game>();
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    using HttpResponseMessage res = await client.GetAsync(baseUrl);
+                    string jsonContent = await res.Content.ReadAsStringAsync();
+                    data = (JsonConvert.DeserializeObject<List<Game>>(jsonContent));
+                    ongoingGames = (from game in data where (int)game.State==1 select game).ToList();
+                }
+                catch { ViewData["ErrCode"] = "-1"; return View(); }
+            }
+            return View(ongoingGames);
         }
     }
 }
